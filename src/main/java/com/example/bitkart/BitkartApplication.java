c80555c3b2e32bfba2244c7a4a2424d6d15f6496
Shubh72074 <Shubhamnishadr16@gmail.com>
2025-03-02 01:33:26 +0530

    java backend project

---
+++ src/main/java/com/example/bitkart/BitkartApplication.java
@@ -0,0 +1,13 @@
+package com.example.bitkart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitkartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitkartApplication.class, args);
+	}
+
+}
